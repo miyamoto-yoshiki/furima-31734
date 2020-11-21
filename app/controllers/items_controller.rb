c6c67f87efc362eab16bac09acb349afed9515a4
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-11-21 18:59:06 +0900

    編集ページの表示、編集成功と失敗による条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_session, only: [:new, :create]
+  before_action :move_to_session, only: [:new, :create, :edit, :update]
 
   def index
     @allitems = Item.all.order('created_at DESC')
@@ -23,6 +23,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.valid?
+      item.save
+      redirect_to item_path
+    else
+      @item = Item.find(params[:id])
+      render :edit
+    end
+  end
+
+
+
   private
 
   def item_params
@@ -32,4 +50,7 @@ class ItemsController < ApplicationController
   def move_to_session
     redirect_to new_user_session_path unless user_signed_in?
   end
+  # def move_to_root
+  #   redirect_to root_path  商品購入機能実装後に実装 売却済み商品のURL入れるとログイン中のユーザー誰でもトップへ
+  # end
 end
