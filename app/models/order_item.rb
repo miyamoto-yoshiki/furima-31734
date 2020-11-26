11b68a868fa3937b615f39046e5fdfa98d0dda99
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-11-26 19:39:12 +0900

    リファクタリング

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem
   include ActiveModel::Model
-  attr_accessor :price, :user_id, :item_id, :post_num, :prefecture_id, :city, :details, :build_num, :phone_num ,:token #ここで受け取る
+  attr_accessor :price, :user_id, :item_id, :post_num, :prefecture_id, :city, :details, :build_num, :phone_num ,:token
 
   with_options presence: true do
     validates :post_num,      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -12,14 +12,10 @@ class OrderItem
   end
 
   def save
-    # アイテムの情報を保存し、「item」という変数に入れている
     item = Item.find(item_id)  #:必要ない
 
-    # 住所の情報を保存
     Address.create(post_num: post_num, prefecture_id: prefecture_id, city: city, details: details, build_num: build_num, phone_num: phone_num, user_id: item.user.id) 
 
-    # 商品の情報を保存
     Order.create(price: item.price, user_id: user_id, item_id: item.id) #user_id: user_idでcurrent取れる
-  
   end
 end
